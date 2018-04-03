aad9fcb950af33348734dd726081bb381ba29ea3
SO4P <37209772+SO4P@users.noreply.github.com>
2018-04-03 23:06:02 +0800

    Add files via upload

---
+++ Solor System/Assets/move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(this.transform.parent.position, new Vector3(1/3,1,0),100 * Time.deltaTime);
+	}
+}
---
+++ Solor System/Assets/move7.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move7 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(this.transform.parent.position, new Vector3(1/2, 1, 0), 30 * Time.deltaTime);
+    }
+}
